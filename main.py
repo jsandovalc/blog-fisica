0859884d96cd90346dc7e014351b98d229a44bf3
Jonathan Sandoval <jsandoval@utp.edu.co>
2017-06-22 11:41:11 -0500

    Serve static files.
    Move statics to static directory.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from sanic import Sanic
-from sanic.response import text
 from sanic_jinja2 import SanicJinja2
 
 app = Sanic(__name__)
 jinja = SanicJinja2(app)
 
-@app.route("/")
-async def test(request):
-    return text('Hello world!')
+app.static('/static', './static')
 
-app.run(host="0.0.0.0", port=8000, debug=True)
+@app.route("/")
+async def index(request):
+    return jinja.render('index.html', request)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
